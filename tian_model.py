97a6735be7eb77705736e0596ed0ab285e73c638
alan <alan.tan.tian@outlook.com>
2020-10-29 11:31:23 +0800

    correct typos in base models. Changed tian model

--- tian_model.py
+++ tian_model.py
@@ -3,9 +3,10 @@ import torch
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader, TensorDataset
 
 
-class TianModel(BaseModel):
+class OneLayerAnnModel(BaseModel):
     @staticmethod
     def preprocessing(train_data_list, validate_data_list, test_data_list):
         # train_data_list = [np.nan_to_num(data[:, :60, :]) for data in train_data_list]
@@ -83,8 +84,85 @@ class TianModel(BaseModel):
         plt.plot(y_pred[:, :].ravel())
         pass
 
+
+class SequentialModel(BaseModel):
+    @staticmethod
+    def preprocessing(train_data_list, validate_data_list, test_data_list):
+        # train_data_list = [np.nan_to_num(data[:, :60, :]) for data in train_data_list]
+        # validate_data_list = [np.nan_to_num(data[:, :60, :]) for data in validate_data_list]
+        # test_data_list = [np.nan_to_num(data[:, :60, :]) for data in test_data_list]
+        train_data_list = [np.nan_to_num(data[:, 30:80, :]) for data in train_data_list]
+        validate_data_list = [np.nan_to_num(data[:, 30:80, :]) for data in validate_data_list]
+        test_data_list = [np.nan_to_num(data[:, 30:80, :]) for data in test_data_list]
+
+        return train_data_list, validate_data_list, test_data_list
+
+    @staticmethod
+    def train_model(x_train, y_train, x_validation, y_validation):
+
+        x_train = x_train.reshape(-1, x_train.shape[1] * x_train.shape[2])
+        y_train = y_train.reshape(-1, y_train.shape[1] * y_train.shape[2])
+
+        x_scalar = MinMaxScaler()
+        x_train = x_scalar.fit_transform(x_train)
+
+        N, D_in, H, D_out = x_train.shape[0], x_train.shape[1], 100, y_train.shape[1]
+        x_train = torch.from_numpy(x_train)
+        y_train = torch.from_numpy(y_train)
+        train_ds = TensorDataset(x_train, y_train)
+        train_dl = DataLoader(train_ds, batch_size=200)
+
+
+        nn_model = torch.nn.Sequential(
+            torch.nn.Linear(D_in, H,  bias=False),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(H, D_out, bias=False),
+        )
+        loss_fn = torch.nn.MSELoss(reduction='sum')
+        learning_rate = 5e-2
+        optimizer = torch.optim.Adam(nn_model.parameters(), lr=learning_rate)
+
+        for epoch in range(5):
+            for xb, yb in train_dl:
+                y_pred = nn_model(xb)
+                loss = loss_fn(y_pred, yb)
+
+                print(epoch, loss.item())
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                # with torch.no_grad():
+                #     if epoch == 0:
+                #         plt.figure()
+                #         # plt.plot(w1.grad)
+                #         plt.plot(yb)
+                #         y_pred_np = y_pred.detach().numpy()
+                #         plt.plot(y_pred_np)
+        return [nn_model, x_scalar]
+
+    @staticmethod
+    def predict(model, x_test):
+        nn_model, x_scalar = model
+        x_test = x_test.reshape(-1, x_test.shape[1] * x_test.shape[2])
+        x_test = x_scalar.transform(x_test)
+
+        x_test = torch.from_numpy(x_test)
+
+        y_pred = nn_model(x_test)
+        y_pred = y_pred.detach().numpy()
+        return y_pred
+
+    @staticmethod
+    def representative_profile_curves(y_test, y_pred, metrics):
+        plt.figure()
+        # plt.plot(w1.grad)
+        plt.plot(y_test[:, :, :].ravel())
+        plt.plot(y_pred[:, :].ravel())
+        pass
+
+
 if __name__ == "__main__":
-    model = TianModel()
+    model = SequentialModel()
     # plt.ion()
     model.param_tuning(range(12), range(12, 13), range(13, 16))
     plt.show()
