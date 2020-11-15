c2ea9585b104c65047a55d9d44ba1b741be66731
DianxinWang <wangdx@sjtu.edu.cn>
2020-11-15 15:29:03 +0800

    Fix bug: cross validation use the same scaler for all iterations

--- base_kam_model.py
+++ base_kam_model.py
@@ -2,6 +2,7 @@ import json
 from typing import List
 import h5py
 import numpy as np
+import prettytable as pt
 from customized_logger import logger as logging
 from const import SUBJECTS, VIDEO_DATA_FIELDS, IMU_DATA_FIELDS, TARGETS_LIST
 import matplotlib.pyplot as plt
@@ -9,11 +10,12 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.utils import shuffle
 from sklearn.metrics import r2_score, mean_squared_error
 
+
 class BaseModel:
     def __init__(self, data_path, x_fields=IMU_DATA_FIELDS + VIDEO_DATA_FIELDS, y_fields=TARGETS_LIST,
                  scalar=MinMaxScaler):
-        logging.info("Create Model from h5 file {}".format(data_path))
-        logging.info("Create Model with input fields {}, output ields {}".format(x_fields, y_fields))
+        logging.info("Load data from h5 file {}".format(data_path))
+        logging.info("Load data with input fields {}, output fields {}".format(x_fields, y_fields))
         self._data_path = data_path
         self._x_fields = x_fields
         self._y_fields = y_fields
@@ -27,34 +29,30 @@ class BaseModel:
         y_field_col_loc = [self.data_columns.index(field_name) for field_name in self._y_fields]
         return data[:, :, x_field_col_loc], data[:, :, y_field_col_loc]
 
-    def param_tuning(self, train_sub_ids: List[int], validate_sub_ids: List[int], test_sub_ids: List[int]):
-        logging.info('Train the model with subject ids: {}'.format(train_sub_ids))
-        logging.info('Test the model with subject ids: {}'.format(test_sub_ids))
-        if len(validate_sub_ids) > 0:
-            logging.info('Validate the model with subject ids: {}'.format(validate_sub_ids))
-        logging.info('\tsubject\t\t\t\t\tR2\t\t\tRMSE\tmean_error\tmean_absolute_error\t\t')
+    def preprocess_train_evaluation(self, train_sub_ids: List[int], validate_sub_ids: List[int], test_sub_ids: List[int]):
+        logging.debug('Train the model with subject ids: {}'.format(train_sub_ids))
+        logging.debug('Test the model with subject ids: {}'.format(test_sub_ids))
+        logging.debug('Validate the model with subject ids: {}'.format(validate_sub_ids))
         model = self.preprocess_and_train(train_sub_ids, validate_sub_ids)
-        self.model_evaluation(model, test_sub_ids)
+        test_results = self.model_evaluation(model, test_sub_ids)
+        return test_results
 
     def cross_validation(self, sub_ids: List[int], test_set_sub_num=1):
         logging.info('Cross validation with subject ids: {}'.format(sub_ids))
         sub_num = len(sub_ids)
         folder_num = int(np.ceil(sub_num / test_set_sub_num))  # the number of cross validation times
-        model_list, test_sub_ids_list = [], []
+        results = []
         for i_folder in range(folder_num):
             test_sub_ids = sub_ids[test_set_sub_num * i_folder:test_set_sub_num * (i_folder + 1)]
-            test_sub_ids_list.append(test_sub_ids)
             train_sub_ids = list(np.setdiff1d(sub_ids, test_sub_ids))
             test_sub_names = [sub_name for sub_index, sub_name in enumerate(SUBJECTS) if sub_index in test_sub_ids]
-            logging.info('Current subjects for test: {}'.format(test_sub_names))
-            model_list.append(self.preprocess_and_train(train_sub_ids, []))
-
-        logging.info('\tsubject\t\t\t\t\tR2\t\t\tRMSE\tmean_error\tmean_absolute_error\t\t')
-        result_all = {}
-        for i_folder in range(folder_num):
-            # separate training and testing so that the printed log would not get mixed.
-            folder_result = self.model_evaluation(model_list[i_folder], test_sub_ids_list[i_folder])
-            result_all.update(folder_result)
+            logging.info('Cross validation: Subjects for test: {}'.format(test_sub_names))
+            results += self.preprocess_train_evaluation(train_sub_ids, [], test_sub_ids)
+        tb = pt.PrettyTable()
+        for test_result in results:
+            tb.field_names = test_result.keys()
+            tb.add_row(test_result.values())
+        print(tb)
 
     def preprocess_and_train(self, train_sub_ids: List[int], validate_sub_ids: List[int]):
         """
@@ -84,18 +82,19 @@ class BaseModel:
     def model_evaluation(self, model, test_sub_ids: List[int]):
         test_sub_names = [sub_name for sub_index, sub_name in enumerate(SUBJECTS) if sub_index in test_sub_ids]
         test_data_list = [self._data_all_sub[sub_name] for sub_name in test_sub_names]
-        test_results = {}
+        test_results = []
         for test_sub_id, test_sub_name in enumerate(test_sub_names):
             test_sub_data = test_data_list[test_sub_id]
             test_sub_x, test_sub_y = self._depart_input_and_output(test_sub_data)
             test_sub_x, test_sub_y = self.preprocess_validation_test_data(test_sub_x, test_sub_y)
             pred_sub_y = self.predict(model, test_sub_x)
-            test_results[test_sub_name] = self.get_all_scores(test_sub_y, pred_sub_y)
-            sub_results = test_results[test_sub_name]
-            logging.info("\t{:17}\t{:7.2f}\t {:7.2f}\t{:7.2f}\t\t{:7.2f}\t\t\t\t\t".format(
-                test_sub_name, sub_results['r2'], sub_results['rmse'], sub_results['mean_error'], sub_results['mean_absolute_error']))
-            self.representative_profile_curves(test_sub_y, pred_sub_y, test_results)
-            self.customized_analysis(test_sub_y, pred_sub_y, test_results)
+            sub_result = {'subject': test_sub_name}
+            sub_result.update(self.get_all_scores(test_sub_y, pred_sub_y))
+            test_results.append(sub_result)
+            logging.debug("\t{:17}\t{:7.2f}\t {:7.2f}\t{:7.2f}\t\t{:7.2f}\t\t\t\t\t".format(
+                test_sub_name, sub_result['r2'], sub_result['rmse'], sub_result['mean_error'], sub_result['mean_absolute_error']))
+            self.representative_profile_curves(test_sub_y, pred_sub_y, "test result for {}".format(test_sub_name))
+            self.customized_analysis(test_sub_y, pred_sub_y, sub_result)
         return test_results
 
     @staticmethod
@@ -143,7 +142,7 @@ class BaseModel:
         return scores
 
     @staticmethod
-    def representative_profile_curves(y_true, y_pred, metrics):
+    def representative_profile_curves(y_true, y_pred, title):
         y_pred = y_pred[:, :, 0]
         y_true = y_true[:, :, 0]
         axis_x = range(y_true.shape[1])
@@ -180,6 +179,6 @@ class BaseModel:
         axs[1, 1].fill_between(axis_x, y_pred_mean - y_pred_std, y_pred_mean + y_pred_std,
                                facecolor='yellow', alpha=0.2)
         axs[1, 1].legend(loc='upper right', fontsize=8)
-        axs[1, 1].set_title("General Predicting status")
+        axs[1, 1].set_title(title)
         plt.tight_layout()
         plt.show(block=False)
--- dianxin_model.py
+++ dianxin_model.py
@@ -2,14 +2,17 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import time
-import random
 from customized_logger import logger as logging
 import tensorflow as tf
+import numpy as np
 from keras import Model, Input
-from keras.layers import GRU, LSTM, Activation, Dense, Masking, Conv1D, Bidirectional, GaussianNoise
+from keras.layers import GRU, LSTM, Activation, Dense, Masking, Conv1D, Bidirectional, GaussianNoise, MaxPooling1D
+from keras.layers import UpSampling1D
+import keras.backend as K
+import keras.losses as Kloss
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.optimizers import Adam
 from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau
 from base_kam_model import BaseModel
 from const import DATA_PATH, SENSOR_LIST, VIDEO_LIST, TARGETS_LIST
@@ -24,28 +27,63 @@ VIDEO_DATA_FIELDS = [VIDEO + "_" + position + "_" + angle for VIDEO in VIDEO_LIS
                      for position in ["x", "y"] for angle in ["90", "180"]]
 
 
-class DXKamModel(BaseModel):
+class DXKamModelWithAutoencoder(BaseModel):
     def __init__(self, data_file, x_fields, y_fields):
-        BaseModel.__init__(self, os.path.join(DATA_PATH, data_file), x_fields, y_fields)
+        BaseModel.__init__(self, os.path.join(DATA_PATH, data_file), x_fields, y_fields, StandardScaler)
         # TODO: feed IMU data fields and Video data fields into two separated network at the beginning.
-        self.model_callback = rnn_model
-        self.model = None
+        self.imu_autoencoder = None
 
     def train_model(self, x_train, y_train, x_validation=None, y_validation=None):
         input_shape = x_train.shape[1:]
-        self.model = self.model_callback(input_shape, LSTM)
-        validation_data = None if x_validation is None else (x_validation, y_validation)
-        self.model.fit(x_train, y_train, validation_data=validation_data, shuffle=True, batch_size=30,
-                       epochs=50, verbose=1, callbacks=[ErrorVisualization(x_validation, y_validation),
-                                                        ModelCheckpoint(os.path.join(DATA_PATH, "val_best.h5"),
-                                                                        monitor='val_loss', verbose=1,
-                                                                        save_best_only=True, mode='max'),
-                                                        ReduceLROnPlateau('val_loss', factor=0.1, patience=5)])
-        return self.model
+        model = rnn_model(input_shape, LSTM)
+        validation_data = None
+        callbacks = []
+        if x_validation is not None:
+            validation_data = (x_validation, y_validation)
+            # callbacks.append(ErrorVisualization(x_validation, y_validation))
+            callbacks.append(ReduceLROnPlateau('val_loss', factor=0.1, patience=5))
+        model.fit(x_train, y_train, validation_data=validation_data, shuffle=True, batch_size=30,
+                       epochs=20, verbose=1, callbacks=callbacks)
+        return model
+
+    def preprocess_train_data(self, x_train, y_train):
+        x_train, y_train = BaseModel.preprocess_train_data(self, x_train, y_train)
+        input_shape = x_train.shape[1:]
+        self.imu_autoencoder = autoencoder(input_shape)
+        self.imu_autoencoder.fit(x_train, x_train, shuffle=True, batch_size=20, epochs=30, verbose=1, callbacks=[PlotVisualization(x_train, x_train)])
+        x_train = np.multiply(self.imu_autoencoder.predict(x_train), (x_train != 0.).all(axis=2)[:, :, None])
+        return x_train, y_train
+
+    def preprocess_validation_test_data(self, x_train, y_train):
+        x_train, y_train = BaseModel.preprocess_train_data(self, x_train, y_train)
+        x_train = np.multiply(self.imu_autoencoder.predict(x_train), (x_train != 0.).all(axis=2)[:, :, None])
+        return x_train, y_train
 
     @staticmethod
     def predict(model, x_test):
-        model.load_weights(os.path.join(DATA_PATH, "val_best.h5"))
+        return model.predict(x_test, verbose=1)
+
+
+class DXKamModel(BaseModel):
+    def __init__(self, data_file, x_fields, y_fields):
+        BaseModel.__init__(self, os.path.join(DATA_PATH, data_file), x_fields, y_fields, StandardScaler)
+        # TODO: feed IMU data fields and Video data fields into two separated network at the beginning.
+
+    def train_model(self, x_train, y_train, x_validation=None, y_validation=None):
+        input_shape = x_train.shape[1:]
+        model = rnn_model(input_shape, LSTM)
+        validation_data = None
+        callbacks = []
+        if x_validation is not None:
+            validation_data = (x_validation, y_validation)
+            callbacks.append(ErrorVisualization(x_validation, y_validation))
+            callbacks.append(ReduceLROnPlateau('val_loss', factor=0.1, patience=5))
+        model.fit(x_train, y_train, validation_data=validation_data, shuffle=True, batch_size=30,
+                       epochs=20, verbose=1, callbacks=callbacks)
+        return model
+
+    @staticmethod
+    def predict(model, x_test):
         return model.predict(x_test, verbose=1)
 
 
@@ -58,7 +96,7 @@ class ErrorVisualization(Callback):
     def on_train_begin(self, logs=None):
         y_true = self.Y_test
         y_pred = self.model.predict(self.X_test)
-        BaseModel.representative_profile_curves(y_true, y_pred, {})
+        BaseModel.representative_profile_curves(y_true, y_pred, "Validation results")
 
     def on_epoch_begin(self, epoch, logs=None):
         y_true = self.Y_test
@@ -70,7 +108,19 @@ class ErrorVisualization(Callback):
     def on_epoch_end(self, epoch, logs=None):
         y_true = self.Y_test
         y_pred = self.model.predict(self.X_test)
-        BaseModel.representative_profile_curves(y_true, y_pred, {})
+        BaseModel.representative_profile_curves(y_true, y_pred, "Validation results")
+
+
+class PlotVisualization(Callback):
+    def __init__(self, x_test, y_test):
+        Callback.__init__(self)
+        self.x_test = x_test
+        self.y_test = y_test
+
+    def on_epoch_end(self, epoch, logs=None):
+        y_true = self.y_test
+        y_pred = self.model.predict(self.x_test)
+        BaseModel.representative_profile_curves(y_true, y_pred, "Validation results")
 
 
 # baseline model
@@ -78,9 +128,9 @@ def rnn_model(input_shape, rnn_layer):
     model = Sequential()
     model.add(Masking(mask_value=0., input_shape=input_shape))
     model.add(GaussianNoise(0.01))
-    model.add(Bidirectional(rnn_layer(96, dropout=0.2, recurrent_dropout=0., return_sequences=True)))
-    model.add(Bidirectional(rnn_layer(30, dropout=0.2, recurrent_dropout=0., return_sequences=True)))
-    model.add(Bidirectional(rnn_layer(15, dropout=0.2, recurrent_dropout=0., return_sequences=True)))
+    model.add(Bidirectional(rnn_layer(96, dropout=0.2, return_sequences=True)))
+    model.add(Bidirectional(rnn_layer(30, dropout=0.2, return_sequences=True)))
+    model.add(Bidirectional(rnn_layer(15, dropout=0.2, return_sequences=True)))
     model.add(Dense(20, use_bias=True, activation='tanh'))
     model.add(Dense(1, use_bias=True))
 
@@ -88,62 +138,40 @@ def rnn_model(input_shape, rnn_layer):
     return model
 
 
-class DXAutoEncoder:
-    def __init__(self, input_shape):
-        encoding_dim = 32
-        input_layer = Input(shape=input_shape)
-        # "encoded" is the encoded representation of the input
-        encoded = Dense(encoding_dim, activation='relu')(input_layer)
-        # "decoded" is the lossy reconstruction of the input
-        decoded = Dense(input_shape, activation='sigmoid')(encoded)
-        # This model maps an input to its reconstruction
-        self.autoencoder = Model(input_layer, decoded)
-        self.encoder = Model(input_layer, encoded)
-        decoded_input = Input(shape=(encoding_dim,))
-        # Retrieve the last layer of the autoencoder model
-        decoder_layer = decoded
-        # Create the decoder model
-        self.decoder = Model(decoded_input, decoder_layer(decoded_input))
+def autoencoder(input_shape):
+    input_ts = Input(shape=input_shape)
+    x = Conv1D(32, 5, activation="relu", padding="same")(input_ts)
+    x = MaxPooling1D(2, strides=2, padding="same")(x)
+    x = Conv1D(16, 5, activation="relu", padding="same")(x)
+    encoded = MaxPooling1D(2, strides=2, padding="same")(x)
+    encoder = Model(input_ts, encoded)
 
-    def encoder(self):
-        return self.encoder
+    x = Conv1D(16, 5, activation="relu", padding="same")(encoded)
+    x = UpSampling1D(2)(x)
+    x = Conv1D(32, 5, activation='relu', padding="same")(x)
+    x = UpSampling1D(2)(x)
+    decoded = Conv1D(input_shape[-1], 1, activation='tanh', padding='same')(x)
 
-    def decoder(self):
-        return self.decoder
+    convolutional_autoencoder = Model(input_ts, decoded)
 
-    def model(self):
-        return self.autoencoder
+    convolutional_autoencoder.summary()
 
-
-class DXKamModelWithAutoencoder(BaseModel):
-    def __init__(self, data_file, x_fields, y_fields):
-        BaseModel.__init__(self, os.path.join(DATA_PATH, data_file), x_fields, y_fields)
-        # TODO: feed IMU data fields and Video data fields into two separated network at the beginning.
-        shape = len(x_fields)
-        self.model = rnn_model(shape, LSTM)
-        self.imu_auto_encoder = DXAutoEncoder()
-
-    def train_model(self, x_train, y_train, x_validation=None, y_validation=None):
-        self.model.fit(x_train, y_train, validation_data=(x_validation, y_validation), shuffle=True, batch_size=20,
-                       epochs=30, verbose=1,
-                       callbacks=[ErrorVisualization(x_validation, y_validation)])
-        return self.model
-
-    def preprocess_train_data(self, x_train, y_train):
-        x_train, y_train = BaseModel.preprocess_train_data(self, x_train, y_train)
-        self.imu_auto_encoder.fit(x_train, x_train)
-        return x_train, y_train
-
-    def preprocess_validation_test_data(self, x_train, y_train):
-        x_train, y_train = BaseModel.preprocess_train_data(self, x_train, y_train)
-        return x_train, y_train
-
-    @staticmethod
-    def predict(model, x_test):
-        return model.predict(x_test, verbose=1)
+    optimizer = "adam"
+    def custom_loss(y_true, y_pred):
+        temp = K.cast(y_true != 0, 'float32')
+        temp = K.flatten(temp)
+        y_true_f = K.flatten(y_true)
+        y_pred_f = K.flatten(y_pred)
+        y_pred_f = y_pred_f * temp
+        return Kloss.mean_absolute_error(y_true_f, y_pred_f)
+    convolutional_autoencoder.compile(optimizer=optimizer, loss=custom_loss)
+    return convolutional_autoencoder
 
 
 if __name__ == "__main__":
-    dx_model = DXKamModel('40samples+stance_swing+drop_negative.h5', IMU_DATA_FIELDS,
+    dx_model = DXKamModel('40samples+stance_swing+kick_out_trunksway.h5', IMU_DATA_FIELDS,
                           TARGETS_LIST)
-    dx_model.param_tuning(range(11), range(11, 13), range(11, 13))
+    # dx_model.param_tuning(range(11), range(11, 13), range(11, 13))
+    dx_model.cross_validation(range(13))
+    # dx_model = DXKamModelWithAutoencoder('40samples+stance_swing+kick_out_trunksway.h5', IMU_DATA_FIELDS, TARGETS_LIST)
+    # dx_model.param_tuning(range(11), range(11, 13), range(11, 13))
