f3b0b372d24684b671396ac18177e2f111f61c84
DianxinWang <wangdx@sjtu.edu.cn>
2021-01-08 15:53:04 +0800

    generate metrics csv

--- figures/table_metrics.py
+++ figures/table_metrics.py
@@ -1,47 +1,51 @@
 import os
 import h5py
 import json
+import csv
+import pandas as pd
+import numpy as np
+from sklearn.metrics import r2_score, mean_squared_error as mse
+from scipy.stats import pearsonr
+from const import TRIALS
+import matplotlib.pyplot as plt
 
 
-# def get_all_scores(y_true, y_pred, y_fields, weights=None):
-#     def get_column_score(arr_true, arr_pred, w):
-#         r2, rmse, mae, r_rmse, sr_rmse, cor_value = [np.zeros(arr_true.shape[0]) for _ in range(6)]
-#         for i in range(arr_true.shape[0]):
-#             arr_true_i = arr_true[i, w[i, :]]
-#             arr_pred_i = arr_pred[i, w[i, :]]
-#
-#             r2[i] = r2_score(arr_true_i, arr_pred_i)
-#             rmse[i] = np.sqrt(mse(arr_true_i, arr_pred_i))
-#             mae[i] = np.mean(abs((arr_true_i - arr_pred_i)))
-#             r_rmse[i] = rmse[i] / (arr_true_i.max() - arr_true_i.min())
-#             sr_rmse[i] = rmse[i] / (0.5 * (arr_true_i.max() - arr_true_i.min() + arr_pred_i.max() - arr_pred_i.min()))
-#             cor_value[i] = pearsonr(arr_true_i, arr_pred_i)[0]
-#
-#         locs = np.where(w.ravel())[0]
-#         r2_all = r2_score(arr_true.ravel()[locs], arr_pred.ravel()[locs])
-#         r2_all = np.full(r2.shape, r2_all)
-#         return {'r2': r2, 'r2_all': r2_all, 'rmse': rmse, 'mae': mae, 'r_rmse': r_rmse, 'sr_rmse': sr_rmse,
-#                 'cor_value': cor_value}
-#
-#     scores = []
-#     weights = {} if weights is None else weights
-#     for output_name, fields in y_fields.items():
-#         for col, field in enumerate(fields):
-#             y_true_one_field = y_true[output_name][:, :, col]
-#             y_pred_one_field = y_pred[output_name][:, :, col]
-#             try:
-#                 weight_one_field = weights[output_name][:, :, col] == 1.
-#             except KeyError:
-#                 weight_one_field = np.full(y_true_one_field.shape, True)
-#                 logging.warning("Use default all true value for {}".format(output_name))
-#             score_one_field = {'output': output_name, 'field': field}
-#             score_one_field.update(get_column_score(y_true_one_field, y_pred_one_field, weight_one_field))
-#             scores.append(score_one_field)
-#     return scores
+def get_score(arr_true, arr_pred, w):
+    assert(len(arr_true.shape) == 1 and arr_true.shape == arr_pred.shape == w.shape)
+
+    locs = np.where(w.ravel())[0]
+    arr_true, arr_pred = arr_true.ravel()[locs], arr_pred.ravel()[locs]
+    plt.figure()
+    plt.plot(arr_true, arr_pred, '.')
+    plt.show()
+    r2 = r2_score(arr_true, arr_pred)
+    rmse = np.sqrt(mse(arr_true, arr_pred))
+    r_rmse = rmse / (arr_true.max() - arr_true.min())
+    cor_value = pearsonr(arr_true, arr_pred)[0]
+    return {'R2': '%.3f' % r2, 'RMSE': '%.3f' % rmse, 'nRMSE': '%.3f' % r_rmse, 'r': '%.3f' % cor_value}
+
 
 if __name__ == '__main__':
-    result_dir = './results/0107_KAM/IMU+OP'
-    with h5py.File(os.path.join(result_dir, 'results.h5'), 'r') as hf:
+    h5_dir = './results/0107_KAM/IMU+OP'
+    with h5py.File(os.path.join(h5_dir, 'results.h5'), 'r') as hf:
         _data_all_sub = {subject: subject_data[:] for subject, subject_data in hf.items()}
         _data_fields = json.loads(hf.attrs['columns'])
-    print(1)
+
+    all_data = np.concatenate(list(_data_all_sub.values()), axis=0)
+    all_data = pd.DataFrame(data=all_data.reshape([-1, all_data.shape[2]]), columns=_data_fields)
+    results = []
+    for trial_index, trial_name in enumerate(TRIALS):
+        trial_name = trial_name.replace('_', ' ')
+        trial_loc = all_data['trial_id'] == trial_index
+        true_value = all_data['true_main_output'][trial_loc]
+        pred_value = all_data['pred_main_output'][trial_loc]
+        weight = all_data['force_phase'][trial_loc]
+        scores = {'category': trial_name, **get_score(true_value, pred_value, weight)}
+        results.append(scores)
+    scores = {'category': 'all', **get_score(all_data['true_main_output'], all_data['pred_main_output'], all_data['force_phase'])}
+    results.append(scores)
+    result_file = os.path.join('./', 'KAM_metrics.csv')
+    with open(result_file, 'w') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(results[0].keys())
+        f_csv.writerows([result.values() for result in results])
