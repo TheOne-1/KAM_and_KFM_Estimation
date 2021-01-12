ba48112578a79861ded8bfc3e6f3fd801d11002c
DianxinWang <wangdx@sjtu.edu.cn>
2021-01-12 11:14:32 +0800

    Minor change in table_metrics

--- figures/table_metrics.py
+++ figures/table_metrics.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import r2_score, mean_squared_error as mse
 from scipy.stats import pearsonr
-from const import TRIALS
+from const import TRIALS, SUBJECTS
 import matplotlib.pyplot as plt
 
 
@@ -20,9 +20,10 @@ def get_score(arr_true, arr_pred, w):
     plt.show()
     r2 = r2_score(arr_true, arr_pred)
     rmse = np.sqrt(mse(arr_true, arr_pred))
-    r_rmse = rmse / (arr_true.max() - arr_true.min())
+    r_rmse = rmse / (arr_true.max() - arr_true.min()) * 100
     cor_value = pearsonr(arr_true, arr_pred)[0]
-    return {'R2': '%.3f' % r2, 'RMSE': '%.3f' % rmse, 'nRMSE': '%.3f' % r_rmse, 'r': '%.3f' % cor_value}
+    rmse = rmse / 9.81 * 100
+    return {'R2': r2, 'RMSE percent': rmse, 'nRMSE percent': r_rmse, 'r':  cor_value}
 
 
 if __name__ == '__main__':
@@ -33,19 +34,26 @@ if __name__ == '__main__':
 
     all_data = np.concatenate(list(_data_all_sub.values()), axis=0)
     all_data = pd.DataFrame(data=all_data.reshape([-1, all_data.shape[2]]), columns=_data_fields)
-    results = []
+    mean_results, std_results = [], []
     for trial_index, trial_name in enumerate(TRIALS):
         trial_name = trial_name.replace('_', ' ')
-        trial_loc = all_data['trial_id'] == trial_index
-        true_value = all_data['true_main_output'][trial_loc]
-        pred_value = all_data['pred_main_output'][trial_loc]
-        weight = all_data['force_phase'][trial_loc]
-        scores = {'category': trial_name, **get_score(true_value, pred_value, weight)}
-        results.append(scores)
-    scores = {'category': 'all', **get_score(all_data['true_main_output'], all_data['pred_main_output'], all_data['force_phase'])}
-    results.append(scores)
-    result_file = os.path.join('./', 'KAM_metrics.csv')
-    with open(result_file, 'w') as f:
-        f_csv = csv.writer(f)
-        f_csv.writerow(results[0].keys())
-        f_csv.writerows([result.values() for result in results])
+        all_subject_results = []
+        for subject_index, subject_name in enumerate(SUBJECTS):
+            trial_loc = (all_data['trial_id'] == trial_index) & (all_data['subject_id'] == subject_index)
+            true_value = all_data['true_main_output'][trial_loc]
+            pred_value = all_data['pred_main_output'][trial_loc]
+            weight = all_data['force_phase'][trial_loc]
+            all_subject_results.append(get_score(true_value, pred_value, weight))
+        all_subject_results = {metric: [subject_result[metric] for subject_result in all_subject_results] for metric in all_subject_results[0].keys()}
+        mean_result = {key:  '%.3f' % np.mean(value) for key, value in all_subject_results.items()}
+        mean_results.append({'category': trial_name, **mean_result})
+        std_result = {key:  '%.3f' %  np.std(value) for key, value in all_subject_results.items()}
+        std_results.append({'category': trial_name, **std_result})
+
+    for export_file, results in [['KAM_mean_metrics.csv', mean_results], ['KAM_std_metrics.csv', std_results]]:
+        result_file = os.path.join('./', export_file)
+        with open(result_file, 'w') as f:
+            f_csv = csv.writer(f)
+            f_csv.writerow(results[0].keys())
+            f_csv.writerows([result.values() for result in results])
+
