5761408d32b639634da1d156ae4192b88571106f
Tian <alan.tan.tian@outlook.com>
2021-01-23 10:41:58 +0800

    figure 5 done

--- figures/PaperFigures.py
+++ figures/PaperFigures.py
@@ -5,6 +5,8 @@ from const import LINE_WIDTH_THICK, FONT_SIZE_LARGE, FORCE_PHASE, SUBJECTS
 from base_kam_model import BaseModel
 import h5py
 import json
+from sklearn.metrics import mean_squared_error as mse
+from scipy.stats import pearsonr
 
 
 def format_axis(line_width=LINE_WIDTH):
@@ -38,12 +40,24 @@ def get_mean_std(data_array, data_fields, col_name):
     return {'true_mean': true_mean, 'true_std': true_std, 'pred_mean': pred_mean, 'pred_std': pred_std}
 
 
-def get_trial_data(file_path, trial_index, subjects=SUBJECTS):
+def get_data(file_path, specific_trial=None, subjects=SUBJECTS):
     with h5py.File(file_path, 'r') as hf:
         _data_all = {subject: subject_data[:] for subject, subject_data in hf.items() if subject in subjects}
         _data_fields = json.loads(hf.attrs['columns'])
 
-    trial_id_col_loc = _data_fields.index('trial_id')
-    trial_data = [data[data[:, 0, trial_id_col_loc] == trial_index, :, :] for data in _data_all.values()]
-    return trial_data, _data_fields
+    if specific_trial is not None:
+        trial_id_col_loc = _data_fields.index('trial_id')
+        _data_all = [data[data[:, 0, trial_id_col_loc] == specific_trial, :, :] for data in _data_all.values()]
+    return _data_all, _data_fields
 
+
+def get_score(arr_true, arr_pred, w):
+    assert(len(arr_true.shape) == 1 and arr_true.shape == arr_pred.shape == w.shape)
+    locs = np.where(w.ravel())[0]
+    arr_true, arr_pred = arr_true.ravel()[locs], arr_pred.ravel()[locs]
+    mae = np.mean(np.abs(arr_true - arr_pred)) / 9.81 * 1000
+    rmse = np.sqrt(mse(arr_true, arr_pred))
+    r_rmse = rmse / (arr_true.max() - arr_true.min()) * 100
+    cor_value = pearsonr(arr_true, arr_pred)[0]
+    rmse = rmse / 9.81 * 1000  # TODO: error-prone code here. Modify generate_combined_data to calculate external KAM.
+    return {'MAE': mae, 'RMSE': rmse, 'rRMSE': r_rmse, 'r':  cor_value}
--- figures/table_metrics.py
+++ figures/table_metrics.py
@@ -4,23 +4,9 @@ import json
 import csv
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error as mse
-from scipy.stats import pearsonr
 from const import TRIALS, SUBJECTS, TRIALS_PRINT
 import matplotlib.pyplot as plt
-
-
-def get_score(arr_true, arr_pred, w):
-    assert(len(arr_true.shape) == 1 and arr_true.shape == arr_pred.shape == w.shape)
-
-    locs = np.where(w.ravel())[0]
-    arr_true, arr_pred = arr_true.ravel()[locs], arr_pred.ravel()[locs]
-    mae = np.mean(np.abs(arr_true - arr_pred)) / 9.81 * 1000
-    rmse = np.sqrt(mse(arr_true, arr_pred))
-    r_rmse = rmse / (arr_true.max() - arr_true.min()) * 100
-    cor_value = pearsonr(arr_true, arr_pred)[0]
-    rmse = rmse / 9.81 * 1000  # TODO: error-prone code here. Modify generate_combined_data to calculate external KAM.
-    return {'MAE': mae, 'RMSE': rmse, 'rRMSE': r_rmse, 'r':  cor_value}
+from PaperFigures import get_score
 
 
 def get_overall_mean_std_result(all_results, metric):
@@ -58,8 +44,9 @@ def get_all_results(h5_dir):
 
 
 if __name__ == '__main__':
+    result_date = 'results/0122_'       # _used_all_the_features_
     for target in ['KAM', 'KFM']:
-        IMU_OP_results, IMU_results, OP_results = [get_all_results('results/0120_' + target + '/' + sensor)
+        IMU_OP_results, IMU_results, OP_results = [get_all_results(result_date + target + '/' + sensor)
                                                    for sensor in ['IMU+OP', 'IMU', 'OP']]
 
         get_trial_result = lambda all_results, trial_name: list(filter(lambda result: result['trial'] == trial_name, all_results))
@@ -78,4 +65,4 @@ if __name__ == '__main__':
             result_df = pd.DataFrame(results)[['MAE', 'RMSE', 'rRMSE', 'r']]
             result_df.columns = [column_name + result_name for column_name in result_df.columns]
             result_df_all_three = pd.concat([result_df_all_three, result_df], axis=1)
-        result_df_all_three.to_csv('./exports/' + target + '_estimation_result_individual.csv', index=False)
+        result_df_all_three.to_csv(result_date + target + '/estimation_result_individual.csv', index=False)
