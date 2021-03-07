21271390a0aa3f130fc5b59fce39dc6ecba14435
Tian <alan.tan.tian@outlook.com>
2021-03-07 22:13:52 +0800

    example data worked

--- README.md
+++ README.md
@@ -8,16 +8,15 @@ We xx The paper is in Submission
 
 ## Environment
 
-The python and lib versions we used are:
+Our environments were:
 Python 3.8; Pytorch 1.7.0; Cuda 11.0; Cudnn 8.0.4;
 
 Versions different from ours may still work.
 
-## Trained models
-
 ## Example data
 
-The example data file contains 10 walking step of 2 subjects. Each subject's data was stored as a 3 dimensional matrix.
+The example data file ([example_data.h5](./trained_models_and_example_data/example_data.h5))
+contains 10 walking step of 2 subjects. Each subject's data was stored as a 3 dimensional matrix.
 The first dimension is walking steps. Its length is 10 because 10 walking steps were provided. The second dimension is
 time step from heel-strike - 20 samples to toe-off + 20 samples. Both heel-strike and toe-off are detected using right
 foot IMU data. Its length is 152, which is the lenghth of the longest step. Zeros were appended in the end of shorter
@@ -38,46 +37,61 @@ _EXT_KM_Y_: external knee moment in frontal plane, also known as knee adduction 
 _EXT_KM_Z_: external knee moment in transverse plane;
 
 #### IMU data
-8 IMUs were placed on each subject.
-IMU field names are in the form of "Measurement(Axis)_Segment". 
+Eight <a href="http://sagemotion.com/" target="_blank"> SageMotion IMUs </a>
+(SageMotion, Kalispell, MT, USA) were placed on each subject.
 For each IMU, its z-axis is aligned with the body segment surface normal,
 y-axis points upwards,
 and x-axis being perpendicular to the y and z axes following the right-hand rule.
 
+IMU fields are named in the form of "Measurement(Axis)_Segment".
 Measurements are _Accel_: acceleration;
 _Gyro_: gyroscope; _Mag_: magnetometer; _Quat_: quaternion.
-
 Segments are _L_FOOT_: left foot; _R_FOOT_: right foot; _R_SHANK_: right shank;
 _R_THIGH_: right thigh; _WAIST_: pelvis; _CHEST_: upper trunk;
 _L_SHANK_: left shank; _L_THIGH_: left thigh
 
 #### Joints detected from camera data
-Two iPhone 11 were placed on the right side (90 degree from walking direction) 
+Two <a href="https://www.apple.com/shop/buy-iphone/iphone-11" target="_blank"> iPhone 11 </a> (Apple Inc., Cupertino, CA, USA)
+
+were placed on the right side (90 degree from walking direction) 
 and back of the subject (180 degree from walking direction). 
 Videos recorded by these two cameras were processed by
 <a href="https://github.com/CMU-Perceptual-Computing-Lab/openpose" target="_blank">OpenPose</a> [1], a body keypoint detection library,
-a body keypoint detection library. Left/right shoulders, left/right hip, mid-hip, left/right knees,
+a body keypoint detection library. 2D positions of left/right shoulders, left/right hip, mid-hip, left/right knees,
 left/right ankles, and left/right heels detected by OpenPose's
 <a href="https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/18de3a0010dd65484b3eb357b5c3679c9a2fdf43/doc/02_output.md" target="_blank">Body_25</a> model.
 
+2D joint position fields are named in the form of "Joint_Axis_Camera".
+For example, _LShoulder_x_90_ means pixel position (1080 pixel in total) of left shoulder joint in horizontal direction of right camera view.
+_RShoulder_y_180_ means pixel position (1920 pixel in total) of right shoulder joint in vertical direction of back camera view.
+
 #### Force plate measurements
+Ground reaction force (GRF) and center of pressure (CoP) were collected with an instrumented treadmill 
+with two split belts (Bertec Corp., Worthington, OH, USA).
+CoP are calibrated with marker trajectories that the left-back corner was the origin of both force plates and optical motion capture.
 The z-axis is aligned with the vertical direction pointing upwards,
-y-axis is aligned with the walking direction pointing forwards,
-and x-axis being perpendicular to the y and z axes following the right-hand rule.
-
-
-_force_x_: anterior o
-CoP are calibrated with marker traj
-plate are cal
+y-axis is aligned with the walking direction
+(in general aligned with anterior-posterior direction) pointing forwards,
+and x-axis being perpendicular to the y and z axes following the right-hand rule
+(in general aligned with medio-lateral direction).
 
 #### Marker trajectories from optical motion capture
-32 markers were placed on each subject.
+32 markers were placed on each subject and collected by an optical motion capture system (Vicon, Oxford Metrics Group, Oxford, UK).
 Markers' field name are in the form of "MarkerName_Axis".
-Marker names' corresponding landmarks are visualized in the following figures.
-![img.png](figures/readme_fig/markers_anterior_view.png)
-![img.png](figures/readme_fig/markers_posterior_view.png)
+Marker names' corresponding body landmarks are visualized in the following figures.
+![img.png](figures/readme_fig/markers_anterior_view.png xxx)
+![img.png](figures/readme_fig/markers_posterior_view.png xxx)
 
-## Running the code
+
+## Use KAM and KFM estimation models
+### Trained models
+Six models are provided:
+Two IMU-based models (e.g. [IMU_based_KAM.pth](./trained_models_and_example_data/IMU_based_KAM.pth)),
+two camera-based models (e.g. [camera_based_KAM.pth](./trained_models_and_example_data/camera_based_KAM.pth)), and
+two IMU & camera fusion model (e.g. [fusion_KAM.pth](./trained_models_and_example_data/fusion_KAM.pth)).
+
+### Running the code
+
 
 ## References
 [1] Z. Cao, G. Hidalgo Martinez, T. Simon, S. Wei, and Y. A. Sheikh,“Openpose: Realtime multi-person 2d pose estimation
--- create_example_data.py
+++ create_example_data.py
@@ -1,56 +1,64 @@
 import h5py
 import os
 import json
-from const import SUBJECTS, ALL_FIELDS
+from const import SUBJECTS, DATA_PATH, EXAMPLE_DATA_FIELDS, VIDEO_LIST
+import torch
+import shutil
+import pandas as pd
+import numpy as np
 
-EXAMPLE_DATA_FIELDS = [
-    'body weight', 'body height', 'force_phase'
 
-    'EXT_KM_X', 'EXT_KM_Y', 'EXT_KM_Z',
+def vid_static_cali():
+    vid_y_90_col_loc = [data_fields.index(marker + '_y_90') for marker in VIDEO_LIST]
+    for sub_name, sub_data in data_all_sub.items():
+        static_side_df = pd.read_csv(DATA_PATH + '/' + sub_name + '/combined/static_side.csv', index_col=0)
+        r_ankle_z = np.mean(static_side_df['RAnkle_y_90'])
+        sub_data[:, :, vid_y_90_col_loc] = sub_data[:, :, vid_y_90_col_loc] - r_ankle_z + 1500
+        data_all_sub[sub_name] = sub_data
 
-    'AccelX_L_FOOT', 'AccelY_L_FOOT', 'AccelZ_L_FOOT', 'GyroX_L_FOOT', 'GyroY_L_FOOT',
-    'GyroZ_L_FOOT', 'MagX_L_FOOT', 'MagY_L_FOOT', 'MagZ_L_FOOT', 'Quat1_L_FOOT', 'Quat2_L_FOOT', 'Quat3_L_FOOT',
-    'Quat4_L_FOOT', 'AccelX_R_FOOT', 'AccelY_R_FOOT', 'AccelZ_R_FOOT', 'GyroX_R_FOOT', 'GyroY_R_FOOT', 'GyroZ_R_FOOT',
-    'MagX_R_FOOT', 'MagY_R_FOOT', 'MagZ_R_FOOT', 'Quat1_R_FOOT', 'Quat2_R_FOOT', 'Quat3_R_FOOT', 'Quat4_R_FOOT',
-    'AccelX_R_SHANK', 'AccelY_R_SHANK', 'AccelZ_R_SHANK', 'GyroX_R_SHANK', 'GyroY_R_SHANK', 'GyroZ_R_SHANK',
-    'MagX_R_SHANK', 'MagY_R_SHANK', 'MagZ_R_SHANK', 'Quat1_R_SHANK', 'Quat2_R_SHANK', 'Quat3_R_SHANK', 'Quat4_R_SHANK',
-    'AccelX_R_THIGH', 'AccelY_R_THIGH', 'AccelZ_R_THIGH', 'GyroX_R_THIGH', 'GyroY_R_THIGH', 'GyroZ_R_THIGH',
-    'MagX_R_THIGH', 'MagY_R_THIGH', 'MagZ_R_THIGH', 'Quat1_R_THIGH', 'Quat2_R_THIGH', 'Quat3_R_THIGH', 'Quat4_R_THIGH',
-    'AccelX_WAIST', 'AccelY_WAIST', 'AccelZ_WAIST', 'GyroX_WAIST', 'GyroY_WAIST', 'GyroZ_WAIST', 'MagX_WAIST',
-    'MagY_WAIST', 'MagZ_WAIST', 'Quat1_WAIST', 'Quat2_WAIST', 'Quat3_WAIST', 'Quat4_WAIST', 'AccelX_CHEST',
-    'AccelY_CHEST', 'AccelZ_CHEST', 'GyroX_CHEST', 'GyroY_CHEST', 'GyroZ_CHEST', 'MagX_CHEST', 'MagY_CHEST',
-    'MagZ_CHEST', 'Quat1_CHEST', 'Quat2_CHEST', 'Quat3_CHEST', 'Quat4_CHEST', 'AccelX_L_SHANK', 'AccelY_L_SHANK',
-    'AccelZ_L_SHANK', 'GyroX_L_SHANK', 'GyroY_L_SHANK', 'GyroZ_L_SHANK', 'MagX_L_SHANK', 'MagY_L_SHANK', 'MagZ_L_SHANK',
-    'Quat1_L_SHANK', 'Quat2_L_SHANK', 'Quat3_L_SHANK', 'Quat4_L_SHANK', 'AccelX_L_THIGH', 'AccelY_L_THIGH',
-    'AccelZ_L_THIGH', 'GyroX_L_THIGH', 'GyroY_L_THIGH', 'GyroZ_L_THIGH', 'MagX_L_THIGH', 'MagY_L_THIGH', 'MagZ_L_THIGH',
-    'Quat1_L_THIGH', 'Quat2_L_THIGH', 'Quat3_L_THIGH', 'Quat4_L_THIGH',
 
-    'LShoulder_x_90', 'LShoulder_x_180',
-    'LShoulder_y_90', 'LShoulder_y_180', 'RShoulder_x_90', 'RShoulder_x_180', 'RShoulder_y_90', 'RShoulder_y_180',
-    'MidHip_x_90', 'MidHip_x_180', 'MidHip_y_90', 'MidHip_y_180', 'RHip_x_90', 'RHip_x_180', 'RHip_y_90', 'RHip_y_180',
-    'LHip_x_90', 'LHip_x_180', 'LHip_y_90', 'LHip_y_180', 'RKnee_x_90', 'RKnee_x_180', 'RKnee_y_90', 'RKnee_y_180',
-    'LKnee_x_90', 'LKnee_x_180', 'LKnee_y_90', 'LKnee_y_180', 'RAnkle_x_90', 'RAnkle_x_180', 'RAnkle_y_90',
-    'RAnkle_y_180', 'LAnkle_x_90', 'LAnkle_x_180', 'LAnkle_y_90', 'LAnkle_y_180', 'RHeel_x_90', 'RHeel_x_180',
-    'RHeel_y_90', 'RHeel_y_180', 'LHeel_x_90', 'LHeel_x_180', 'LHeel_y_90', 'LHeel_y_180',
-
-    'plate_2_force_x', 'plate_2_force_y', 'plate_2_force_z', 'plate_2_cop_x', 'plate_2_cop_y', 'plate_2_cop_z',
-
-    'LFCC_X', 'LFM5_X',
-    'LFM2_X', 'RFCC_X', 'RFM5_X', 'RFM2_X', 'LTAM_X', 'LFAL_X', 'LSK_X', 'LTT_X', 'RTAM_X', 'RFAL_X', 'RSK_X', 'RTT_X',
-    'LFME_X', 'LFLE_X', 'LTH_X', 'LFT_X', 'RFME_X', 'RFLE_X', 'RTH_X', 'RFT_X', 'LIPS_X', 'RIPS_X', 'LIAS_X', 'RIAS_X',
-    'MAI_X', 'SXS_X', 'SJN_X', 'CV7_X', 'LAC_X', 'RAC_X', 'LFCC_Y', 'LFM5_Y', 'LFM2_Y', 'RFCC_Y', 'RFM5_Y', 'RFM2_Y',
-    'LTAM_Y', 'LFAL_Y', 'LSK_Y', 'LTT_Y', 'RTAM_Y', 'RFAL_Y', 'RSK_Y', 'RTT_Y', 'LFME_Y', 'LFLE_Y', 'LTH_Y', 'LFT_Y',
-    'RFME_Y', 'RFLE_Y', 'RTH_Y', 'RFT_Y', 'LIPS_Y', 'RIPS_Y', 'LIAS_Y', 'RIAS_Y', 'MAI_Y', 'SXS_Y', 'SJN_Y', 'CV7_Y',
-    'LAC_Y', 'RAC_Y', 'LFCC_Z', 'LFM5_Z', 'LFM2_Z', 'RFCC_Z', 'RFM5_Z', 'RFM2_Z', 'LTAM_Z', 'LFAL_Z', 'LSK_Z', 'LTT_Z',
-    'RTAM_Z', 'RFAL_Z', 'RSK_Z', 'RTT_Z', 'LFME_Z', 'LFLE_Z', 'LTH_Z', 'LFT_Z', 'RFME_Z', 'RFLE_Z', 'RTH_Z', 'RFT_Z',
-    'LIPS_Z', 'RIPS_Z', 'LIAS_Z', 'RIAS_Z', 'MAI_Z', 'SXS_Z', 'SJN_Z', 'CV7_Z', 'LAC_Z', 'RAC_Z']
-
+"""Create data file"""
 with h5py.File(os.environ.get('KAM_DATA_PATH') + '/40samples+stance.h5', 'r') as hf:
     data_all_sub = {subject: subject_data[:] for subject, subject_data in hf.items()}
     data_fields = json.loads(hf.attrs['columns'])
     export_path = 'trained_models_and_example_data/example_data.h5'
     example_col_loc = [data_fields.index(column) for column in EXAMPLE_DATA_FIELDS]
+    vid_static_cali()
     with h5py.File(export_path, 'w') as hf:
-        hf.create_dataset('subject_01', data=data_all_sub[SUBJECTS[7]][:10, :, example_col_loc], dtype='float32')
-        hf.create_dataset('subject_02', data=data_all_sub[SUBJECTS[8]][:10, :, example_col_loc], dtype='float32')
-        hf.attrs['columns'] = json.dumps(data_fields)
+        hf.create_dataset('subject_01', data=data_all_sub[SUBJECTS[5]][200:210, :, example_col_loc], dtype='float32')
+        hf.create_dataset('subject_02', data=data_all_sub[SUBJECTS[6]][200:210, :, example_col_loc], dtype='float32')
+        hf.attrs['columns'] = json.dumps(EXAMPLE_DATA_FIELDS)
+
+
+"""Create model file"""
+test_name = '0301_test_'
+subject_folder = 's002_wangdianxin'
+for moment in ['KAM', 'KAM']:
+    # for model, model_name in zip(['8IMU_2camera', '8IMU', '2camera'], ['fusion', 'IMU_based', 'camera_based']):
+    for model, model_name in zip(['8IMU_2camera'], ['fusion']):
+        model_path_src = os.path.join(DATA_PATH, 'training_results', test_name+moment, model, 'sub_models',
+                                      subject_folder, 'four_source_model.pth')
+        model_path_dest = './trained_models_and_example_data/' + model_name + '_' + moment + '.pth'
+        shutil.copyfile(model_path_src, model_path_dest)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
