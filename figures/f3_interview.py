5496bfde3829ab7b988c9cc38a86b1d6136fc9d3
TheOne-1 <alan.tan.tian@outlook.com>
2021-06-11 16:12:04 +0800

    updated f9

---
+++ figures/f3_interview.py
@@ -0,0 +1,73 @@
+import os
+import h5py
+import json
+from figures.PaperFigures import get_mean_std, format_axis
+from const import SUBJECTS
+import numpy as np
+from const import LINE_WIDTH, FONT_DICT_LARGE, FONT_SIZE, FONT_DICT, FONT_SIZE, FONT_DICT_LARGE
+from figures.f6 import save_fig
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+
+def draw_f3(mean_std_kam, mean_std_kfm):
+    def draw_subplot(ax, mean_std):
+        arr_true_mean, arr_true_std, arr_pred_mean, arr_pred_std = mean_std['true_mean'], mean_std['true_std'], \
+                                                                   mean_std['pred_mean'], mean_std['pred_std']
+        axis_x = range(arr_true_mean.shape[0])
+        color_0, color_1 = np.array([255, 166, 0]) / 255, np.array([0, 103, 137]) / 255
+        ax.plot(axis_x, arr_true_mean, color=color_0, label='Ground-Truth Moment', linewidth=LINE_WIDTH*2)
+        ax.fill_between(axis_x, arr_true_mean - arr_true_std, arr_true_mean + arr_true_std,
+                        facecolor=color_0, alpha=0.4)
+        ax.plot(axis_x, arr_pred_mean, '--', color=color_1, label='Estimated Moment', linewidth=LINE_WIDTH*2)
+        ax.fill_between(axis_x, arr_pred_mean - arr_pred_std, arr_pred_mean + arr_pred_std,
+                        facecolor=color_1, alpha=0.4)
+        ax.tick_params(labelsize=FONT_DICT_LARGE['fontsize'])
+        ax.set_xticks(range(0, 101, 25))
+        ax.set_xticklabels(range(0, 101, 25), fontdict=FONT_DICT_LARGE)
+        ax.set_xlabel('Stance Phase (%)', fontdict=FONT_DICT_LARGE)
+        ax.set_xlim(0, 100)
+        format_axis()
+
+    def subplot_1_style():
+        ax = plt.gca()
+        ax.set_ylabel('KAM (%BW$\cdot$BH)', fontdict=FONT_DICT_LARGE, labelpad=5)
+        ax.set_ylim(-2, 4)
+        ticks = [-2, 0, 2, 4]
+        ax.set_yticks(ticks)
+        ax.set_yticklabels(ticks, fontdict=FONT_DICT_LARGE)
+
+    def subplot_2_style():
+        ax = plt.gca()
+        ax.set_ylabel('KFM (%BW$\cdot$BH)', fontdict=FONT_DICT_LARGE, labelpad=5)
+        ax.set_ylim(-2.5, 7.5)
+        ticks = [-2.5, 0, 2.5, 5, 7.5]
+        ax.set_yticks(ticks)
+        ax.set_yticklabels(ticks, fontdict=FONT_DICT_LARGE)
+
+    rc('font', family='Arial')
+    fig = plt.figure(figsize=(7, 9))
+    gs = gridspec.GridSpec(nrows=3, ncols=1, height_ratios=[1, 10, 10])        # , width_ratios=[8, 1, 8]
+    draw_subplot(fig.add_subplot(gs[1, 0]), mean_std_kam)
+    subplot_1_style()
+    draw_subplot(fig.add_subplot(gs[2, 0]), mean_std_kfm)
+    subplot_2_style()
+    plt.tight_layout(rect=[0., -0., 1., 1.0], w_pad=3)
+    plt.legend(bbox_to_anchor=(0.9, 2.9), ncol=1, fontsize=FONT_DICT_LARGE['fontsize'])
+    save_fig('c5_overall')
+
+
+if __name__ == "__main__":
+    with h5py.File('results/0326KAM/8IMU_2camera/results.h5', 'r') as hf:
+        kam_data_all_sub = {subject: subject_data[:] for subject, subject_data in hf.items()}
+        kam_data_fields = json.loads(hf.attrs['columns'])
+    with h5py.File('results/0326KFM/8IMU_2camera/results.h5', 'r') as hf:
+        kfm_data_all_sub = {subject: subject_data[:] for subject, subject_data in hf.items()}
+        kfm_data_fields = json.loads(hf.attrs['columns'])
+
+    kam_mean_std = get_mean_std(np.concatenate(list(kam_data_all_sub.values()), axis=0), kam_data_fields, 'main_output')
+    kfm_mean_std = get_mean_std(np.concatenate(list(kfm_data_all_sub.values()), axis=0), kfm_data_fields, 'main_output')
+    kfm_mean_std['true_mean'], kfm_mean_std['pred_mean'] = -kfm_mean_std['true_mean'], -kfm_mean_std['pred_mean']
+    draw_f3(kam_mean_std, kfm_mean_std)
+    plt.show()
--- figures/f9_interview.py
+++ figures/f9_interview.py
@@ -2,7 +2,7 @@ import h5py
 import json
 import pandas as pd
 import numpy as np
-from const import LINE_WIDTH, FONT_DICT_SMALL, SUBJECTS, TRIALS
+from const import LINE_WIDTH, GRAVITY, FONT_DICT_SMALL, SUBJECTS, TRIALS
 from figures.f6 import save_fig
 from figures.PaperFigures import get_peak_of_each_gait_cycle, format_axis, get_mean_gait_cycle_then_find_peak
 import matplotlib.pyplot as plt
@@ -11,28 +11,33 @@ import matplotlib.lines as lines
 from more_itertools import sort_together
 from scipy.stats import ttest_rel
 import matplotlib.gridspec as gridspec
-import matplotlib.patches as patches
 
 
 def draw_sigifi_sign(mean_, std_, bar_locs, p_between_pattern, ylim):
     one_two, two_three, one_three = p_between_pattern
+    lo = [0. for i in range(3)]        # line offset
+    for i_bar, there_are_two_lines in enumerate([one_two and one_three, one_two and two_three, two_three and one_three]):
+        if there_are_two_lines:
+            lo[i_bar] = 0.1
     y_top = max([a + b for a, b in zip(mean_, std_)])
-    top_lines = [y_top + 0.07 * ylim, y_top + 0.2 * ylim, y_top + 0.33 * ylim]
-    for i_pair, [pair, loc_0, loc_1] in enumerate(zip([one_two, two_three, one_three], [0, 1, 0], [1, 2, 2])):
-        if not pair[0] and not pair[1]: continue
-        top_line = top_lines.pop(0)
+
+    for pair, loc_0, loc_1 in zip([one_two, two_three, one_three], [0, 1, 0], [1, 2, 2]):
+        if not pair: continue
         if loc_0 == 0 and loc_1 == 2:
+            lo_sign = -1
+            if not one_two and not two_three:
+                top_line = y_top + 0.07 * ylim
+            else:
+                top_line = y_top + 0.22 * ylim
             coe_0, coe_1 = 0.53, 0.47
         else:
+            lo_sign = 1
+            top_line = y_top + 0.08 * ylim
             coe_0, coe_1 = 0.56, 0.44
-        if pair[0]:
-            plt.plot([bar_locs[2*loc_0], bar_locs[2*loc_1]], [top_line, top_line], color=color_0, linewidth=LINE_WIDTH)
-        if pair[1]:
-            plt.plot([bar_locs[2*loc_0+1], bar_locs[2*loc_1+1]], [top_line-0.025*ylim, top_line-0.025*ylim],
-                     color=color_1, linewidth=LINE_WIDTH)
-        plt.text(bar_locs[2*loc_0]*coe_0 + bar_locs[2 * loc_1 + 1] * coe_1, top_line - 0.11 * ylim, '*', fontdict={'fontname': 'Times New Roman'}, size=35, zorder=20)
-        rect = patches.Rectangle((bar_locs[2*loc_0]*coe_0 + bar_locs[2 * loc_1 + 1] * coe_1, top_line - 0.11 * ylim), 0.47, 0.15*ylim, linewidth=0, color='white', zorder=10)
-        ax.add_patch(rect)
+        diff_line_0x = [bar_locs[loc_0]+lo_sign*lo[loc_0], bar_locs[loc_0]+lo_sign*lo[loc_0], bar_locs[loc_1]-lo_sign*lo[loc_1], bar_locs[loc_1]-lo_sign*lo[loc_1]]
+        diff_line_0y = [mean_[loc_0] + std_[loc_0] + 0.04 * ylim, top_line, top_line, mean_[loc_1] + std_[loc_1] + 0.04 * ylim]
+        plt.plot(diff_line_0x, diff_line_0y, 'black', linewidth=LINE_WIDTH)
+        plt.text((bar_locs[loc_0]+lo_sign*lo[loc_0])*coe_0 + (bar_locs[loc_1]-lo_sign*lo[loc_1])*coe_1, top_line - 0.04 * ylim, '*', color='black', size=40)
 
 
 def format_errorbar_cap(caplines, size=15):
@@ -52,15 +57,15 @@ def sort_gait_cycles_according_to_param(param_config, sub_param, sub_data):
 
 def init_f9():
     rc('font', family='Arial')
-    fig = plt.figure(figsize=(16, 7))
-    gs = gridspec.GridSpec(nrows=2, ncols=1, height_ratios=[5, 5])        # , width_ratios=[8, 1, 8]
+    fig = plt.figure(figsize=(9, 5))
+    gs = gridspec.GridSpec(nrows=1, ncols=2, width_ratios=[5, 5])        # , width_ratios=[8, 1, 8]
     return fig, gs
 
 
 def draw_f9_subplot(mean_, std_, p_between_pattern, ax, moment_name):
     def format_kam_y_ticks():
-        ax.set_ylabel('Peak KAM (%BW$\cdot$BH)', fontdict=FONT_DICT_SMALL)
-        ylim = 8
+        ax.set_ylabel('1st Peak KAM (%BW$\cdot$BH)', fontdict=FONT_DICT_SMALL)
+        ylim = 6
         ax.set_ylim(0, ylim)
         ax.set_yticks(range(0, ylim+1, 2))
         ax.set_yticklabels(range(0, ylim+1, 2), fontdict=FONT_DICT_SMALL)
@@ -68,26 +73,25 @@ def draw_f9_subplot(mean_, std_, p_between_pattern, ax, moment_name):
 
     def format_kfm_y_ticks():
         ax.set_ylabel('Peak KFM (%BW$\cdot$BH)', fontdict=FONT_DICT_SMALL)
-        ylim = 12
+        ylim = 10
         ax.set_ylim(0, ylim)
         ax.set_yticks(range(0, ylim+1, 2))
         ax.set_yticklabels(range(0, ylim+1, 2), fontdict=FONT_DICT_SMALL)
         return ylim
 
     def format_x_ticks():
-        ax.set_xlabel(' Foot Progression Angle                                             Step Width          '
-                      '                                        Trunk Sway Angle    ', fontdict=FONT_DICT_SMALL, labelpad=5)
-        ax.set_xlim(-1, 26)
-        ax.set_xticks(np.arange(0.5, 25, 3))
-        ax.set_xticklabels(['Toe-in', 'Normal', 'Toe-out', 'Narrow', 'Normal', 'Wide', 'Small', 'Medium', 'Large'],
+        ax.set_xlabel('Foot Progression Angle', fontdict=FONT_DICT_SMALL, labelpad=5)
+        ax.set_xlim(-1, 8)
+        ax.set_xticks(np.arange(0.5, 7, 3))
+        ax.set_xticklabels(['Toe-in', 'Normal', 'Toe-out'],
                            fontdict=FONT_DICT_SMALL, linespacing=0.95)
 
     format_axis()
     format_x_ticks()
-    bar_locs = [x + y for x in range(0, 27, 3) for y in [0, 1]]
+    bar_locs = [x + y for x in range(0, 9, 3) for y in [0, 1]]
+    color_0, color_1 = np.array([255, 166, 0]) / 255, np.array([0, 103, 137]) / 255
     colors = [color_0, color_1] * int(len(bar_locs) / 2)
-    bar_ = []
-    for i_condition in range(len(bar_locs)):
+    for i_condition in range(len(mean_)):
         bar_.append(plt.bar(bar_locs[i_condition], mean_[i_condition], color=colors[i_condition], width=1))
     ebar, caplines, barlinecols = plt.errorbar(bar_locs, mean_, std_,
                                                capsize=0, ecolor='black', fmt='none', lolims=True,
@@ -95,26 +99,27 @@ def draw_f9_subplot(mean_, std_, p_between_pattern, ax, moment_name):
     format_errorbar_cap(caplines, 20)
     if moment_name == 'KAM':
         ylim = format_kam_y_ticks()
-        plt.legend(bar_[0:2], ['Laboratory Force Plate & Optical Motion Capture', 'Portable IMUs & Smartphone Cameras (Proposed Fusion Model)'],
-            handlelength=3, bbox_to_anchor=(0.42, 1.08), ncol=1, fontsize=FONT_DICT_SMALL['fontsize'], frameon=False)
     elif moment_name == 'KFM':
         ylim = format_kfm_y_ticks()
-    for i_trial, trial_name in enumerate(['fpa', 'step_width', 'trunk_sway']):
-        draw_sigifi_sign(mean_[6*i_trial:6*i_trial+6], std_[6*i_trial:6*i_trial+6], bar_locs[6*i_trial:6*i_trial+6],
+    for i_trial, trial_name in enumerate(['fpa']):
+        index = [i_trial*6, i_trial*6 + 2, i_trial*6 + 4]
+        draw_sigifi_sign([mean_[x] for x in index], [std_[x] for x in index], [bar_locs[x]+0.5 for x in index],
                          p_between_pattern[trial_name], ylim)
-        plt.tight_layout(rect=[0., -0.01, 1, 1.01], w_pad=2, h_pad=1)
 
 
 def finalize_f9(fig):
-    l1 = lines.Line2D([0.365, 0.365], [0.01, 0.85], linestyle='--', transform=fig.transFigure, color='gray')
-    l2 = lines.Line2D([0.677, 0.677], [0.01, 0.85], linestyle='--', transform=fig.transFigure, color='gray')
-    fig.lines.extend([l1, l2])
-    save_fig('f9', 600)
+    # pass
+    plt.tight_layout(rect=[0, 0, 1, 0.9], w_pad=1, h_pad=1)
+    plt.legend(bar_[0:2], ['Ground-Truth Moment', 'Estimated Moment'],
+               bbox_to_anchor=(0.6, 1.24), ncol=2, fontsize=FONT_DICT_SMALL['fontsize'])
+    # l1 = lines.Line2D([0.372, 0.372], [0.01, 0.85], linestyle='--', transform=fig.transFigure, color='gray')
+    # l2 = lines.Line2D([0.682, 0.682], [0.01, 0.85], linestyle='--', transform=fig.transFigure, color='gray')
+    # fig.lines.extend([l1, l2])
+    # save_fig('c5_gait_patterns')
 
 
 if __name__ == "__main__":
     data_path = 'D:\Tian\Research\Projects\VideoIMUCombined\experiment_data\KAM\\'
-    color_0, color_1 = np.array([255, 166, 0]) / 255, np.array([0, 103, 137]) / 255       # [255, 166, 0]
     result_date = 'results/0326'
     with h5py.File(result_date + 'KAM/8IMU_2camera/results.h5', 'r') as hf:
         kam_data_all_sub = {subject: subject_data[:] for subject, subject_data in hf.items()}
@@ -137,10 +142,11 @@ if __name__ == "__main__":
     fpa_config = {'trial_name': 'fpa', 'pattern_names': ('toe_in', 'normal_fpa', 'toe_out')}
     ts_config = {'trial_name': 'trunk_sway', 'pattern_names': ('small_ts', 'normal_ts', 'large_ts')}
     sw_config = {'trial_name': 'step_width', 'pattern_names': ('small_sw', 'normal_sw', 'large_sw')}
+    bar_ = []
     fig, gs = init_f9()
     peak_of_gait_df = pd.DataFrame()
     # compute results
-    for config in [fpa_config, ts_config, sw_config]:
+    for config in [fpa_config]:
         for data, data_fields, sign, moment_name in zip([kam_data_all_sub, kfm_data_all_sub], [kam_data_fields, kfm_data_fields], [1, -1], ['KAM', 'KFM']):
             for i_subject, subject in enumerate(SUBJECTS):
                 sub_param, sub_data = gait_param_all_sub[subject], data[subject]
@@ -149,7 +155,7 @@ if __name__ == "__main__":
                 small_param, normal_param, large_param = np.array_split(param_sorted, 3)
                 for steps, param, pattern_name in zip((small_steps, normal_steps, large_steps), (small_param, normal_param, large_param), config['pattern_names']):
                     # get_mean_gait_cycle_then_find_peak get_peak_of_each_gait_cycle
-                    true_peak, pred_peak = get_peak_of_each_gait_cycle(np.stack(steps) * sign, data_fields, 0.5)
+                    true_peak, pred_peak = get_mean_gait_cycle_then_find_peak(np.stack(steps) * sign, data_fields, 0.5)
                     peak_of_gait_df = peak_of_gait_df.append(
                         [[i_subject, config['trial_name'], pattern_name, moment_name, np.mean(param),
                           bl_param_df[config['trial_name']][i_subject], np.mean(true_peak), np.mean(pred_peak)]])
@@ -159,8 +165,8 @@ if __name__ == "__main__":
     for i_moment, moment_name in enumerate(['KAM', 'KFM']):
         moment_average, moment_std = [], []
         print(config)
-        p_between_pattern = {config['trial_name']: [[False, False], [False, False], [False, False]] for config in [fpa_config, ts_config, sw_config]}
-        for config in [fpa_config, sw_config, ts_config]:
+        p_between_pattern = {config['trial_name']: [False, False, False] for config in [fpa_config, ts_config, sw_config]}
+        for config in [fpa_config]:
             config_df = peak_of_gait_df[(peak_of_gait_df['moment'] == moment_name) & (peak_of_gait_df['trial_name'] == config['trial_name'])]
             for pattern_name in config['pattern_names']:
                 pattern_df = config_df[config_df['pattern'] == pattern_name]
@@ -169,14 +175,17 @@ if __name__ == "__main__":
                 p_between_models = round(ttest_rel(pattern_df['true_peak'], pattern_df['pred_peak']).pvalue, 3)
                 print(p_between_models)
             for i_pair, pattern_pair in enumerate([(0, 1), (1, 2), (0, 2)]):
-                pattern_0_results = config_df[config_df['pattern'] == config['pattern_names'][pattern_pair[0]]]
-                pattern_1_results = config_df[config_df['pattern'] == config['pattern_names'][pattern_pair[1]]]
-                p_true = ttest_rel(pattern_0_results['true_peak'].values, pattern_1_results['true_peak'].values).pvalue
-                p_pred = ttest_rel(pattern_0_results['pred_peak'].values, pattern_1_results['pred_peak'].values).pvalue
-                print('P between {} and {} is {} (ground-truth) and {} (prediction)'.format(
-                    config['pattern_names'][pattern_pair[0]], config['pattern_names'][pattern_pair[1]], round(p_true, 3), round(p_pred, 3)))
-                p_between_pattern[config['trial_name']][i_pair] = p_true < 0.05, p_pred < 0.05
-        ax = fig.add_subplot(gs[i_moment, 0])
+                p_val_truth = ttest_rel(
+                    config_df[config_df['pattern'] == config['pattern_names'][pattern_pair[0]]]['true_peak'].values,
+                    config_df[config_df['pattern'] == config['pattern_names'][pattern_pair[1]]]['true_peak'].values).pvalue
+                print('P between {} and {} is {}'.format(config['pattern_names'][pattern_pair[0]], config['pattern_names'][pattern_pair[1]], round(p_val_truth, 3)))
+                p_val_esti = ttest_rel(
+                    config_df[config_df['pattern'] == config['pattern_names'][pattern_pair[0]]]['pred_peak'].values,
+                    config_df[config_df['pattern'] == config['pattern_names'][pattern_pair[1]]]['pred_peak'].values).pvalue
+                print('P between {} and {} is {}'.format(config['pattern_names'][pattern_pair[0]], config['pattern_names'][pattern_pair[1]], round(p_val_esti, 3)))
+                if p_val_truth < 0.05:
+                    p_between_pattern[config['trial_name']][i_pair] = True
+        ax = fig.add_subplot(gs[0, i_moment])
         draw_f9_subplot(moment_average, moment_std, p_between_pattern, ax, moment_name)
     finalize_f9(fig)
     plt.show()
